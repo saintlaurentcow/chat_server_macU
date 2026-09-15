22d316edb92cd4f3928547a4e7f35e5e6e870c73
Sri Sharada Kalyanaraman <srisharada.kalyanaraman@students.mq.edu.au>
2026-09-15 18:37:09 +1000

    added starting plants

--- project/COMP2000-Simulation/src/App.java
+++ project/COMP2000-Simulation/src/App.java
@@ -4,11 +4,9 @@ public class App {
     public static void main(String[] args) {
         Window window = new Window();
         Controller controller = new Controller(window);// Starts controller and weather timer
-        int delay = 50; //Refresh 20 times per second   (huh??? twenty? how? it's initialised to fifty, did you mean 40? - Allie) 
-        //TODO: needs refresh rate code here or in controller - Allie
-        //nm did it in controller, ignore the above - Allie
-
-
+       
         new Sunflower(new Point(200, 200), window);
+        new Sunflower(new Point(400, 250), window);
+        new Weeds(new Point(600, 200), window);
     }
 }
