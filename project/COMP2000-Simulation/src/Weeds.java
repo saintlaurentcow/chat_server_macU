6b90c7471140a9c8b415e1aecfb979273ef2640c
Sri Sharada Kalyanaraman <srisharada.kalyanaraman@students.mq.edu.au>
2026-09-15 18:32:23 +1000

    changes to weedss

--- project/COMP2000-Simulation/src/Weeds.java
+++ project/COMP2000-Simulation/src/Weeds.java
@@ -2,49 +2,57 @@ import java.awt.*;
 
 
 public class Weeds extends Plant{
-    int size;
 
     Weeds(Point position, Window window){
         super(position, window);
-        this.position = position;
         this.setBackground(Color.darkGray);
     }
 
+    @Override 
+    protected int getInitialGrowthDelay(){
+        return 1500;
+    }
+
+    @Override 
+    protected int getInitialSpreadNum(){
+        return 4;
+    }
+
+    @Override 
+    protected boolean scalesWithGrowth(){
+        return false;
+    }
+
     @Override
-
-    public void spread(){
+     public void spread(){
         Radius radius = new Radius(position, spreadRadius); //Typo in spread, fixed it - Allie
-
         for(int i = 0; i < spreadNum; i++){ //your r key might be broken, Sri. Might want to take a look at that. Also int i, not int o - Allie
             Point newPoint = radius.getRandomPoint();
-            @SuppressWarnings("unused") //size is an int type that you are trying to call as a Window type. I don't know what this does, so for now I'm supressing this. - Allie
-            Weeds child = new Weeds(newPoint, window); //See above, remove line 20 when you've fixed this. 
+            new Weeds(newPoint, window);
         }
     }
 
     @Override
     void seedAction() {
-        throw new UnsupportedOperationException("Not supported yet."); //Added an exception to prevent it throwing an error - Allie
     }
 
     @Override
     void seedlingAction() {
-        throw new UnsupportedOperationException("Not supported yet."); //See above - Allie
     }
 
     @Override
     void juvenileAction() {
-        throw new UnsupportedOperationException("Not supported yet."); //Ditto - Allie
     }
 
     @Override
     void adultAction() {
-        throw new UnsupportedOperationException("Not supported yet."); //Fourth verse same as the first - Allie
+        if ((int) (Math.random() * 100) < 3){
+            spread();
+        }
     }
 
     @Override
     void deadAction() {
-        throw new UnsupportedOperationException("Not supported yet."); //You get the idea - Allie
     }
 
     
