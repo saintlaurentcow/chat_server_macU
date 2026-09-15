0f0f86e383d50244c26ac846180e58e71f969356
Sri Sharada Kalyanaraman <srisharada.kalyanaraman@students.mq.edu.au>
2026-09-15 20:16:00 +1000

    sunflower and weeds interaction

--- project/COMP2000-Simulation/src/Sunflower.java
+++ project/COMP2000-Simulation/src/Sunflower.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sunflower extends Flower{
     //int size;
@@ -14,6 +16,32 @@ public class Sunflower extends Flower{
     @Override 
     protected Color getBloomColor(){
         return Color.yellow;
+    }
+
+    @Override 
+    public void tick(){
+        super.tick();
+        checkWeedOvercrowding(); 
+    }
+
+    private void checkWeedOvercrowding(){
+        if(growthState == DEAD){
+            return;
+        }
+
+        List<Weeds> touchingWeeds = new ArrayList<>();
+        for(Plant p : window.getPlants()){
+            if(p instanceof Weeds w && this.getBounds().intersects(p.getBounds())){
+                touchingWeeds.add(w);
+            }
+        }
+
+        if(touchingWeeds.size() > 10){
+            this.growthState = DEAD;
+            for(Weeds w : touchingWeeds){
+                w.growthState = DEAD;
+            }
+        }
     }
 
     @Override
