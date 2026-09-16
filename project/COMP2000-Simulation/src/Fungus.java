6b499a566bda4cdd4ce21b188c018e7d4b882811
Sri Sharada Kalyanaraman <srisharada.kalyanaraman@students.mq.edu.au>
2026-09-16 13:31:41 +1000

    deleted fungus class (for now)

--- project/COMP2000-Simulation/src/Fungus.java
+++
@@ -1,9 +0,0 @@
-/*
-    Mushrooms need shade to grow, so they might pop up if there are a lot
-    of overcast days or if we add shaded areas
-*/
-import java.awt.*;
-
-abstract class Fungus extends Plant{
-    
-}
